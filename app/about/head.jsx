1021eaf57e77150553d1fbf588103f8e3b21d608
Atharva Pardeshi <atharva.exe@gmail.com>
2023-04-04 00:42:03 +0530

    feat: add head

---
+++ app/about/head.jsx
@@ -0,0 +1,11 @@
+export default function AboutHead() {
+	return (
+		<>
+			<title>About Atharva</title>
+			<meta
+				name="description"
+				content="Atharva is a self-taught software developer who loves to learn"
+			/>
+		</>
+	)
+}
